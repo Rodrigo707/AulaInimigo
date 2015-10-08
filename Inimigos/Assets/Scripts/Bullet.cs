707ac04bbf517f6d0ba8d86f4b639c6eb7104902
Rodrigo707 <rodigo.silva94@gmail.com>
2015-10-07 22:38:31 -0300

    Subindo projeto, falta destruir a bala ao acertar o inimigo.

---
+++ Inimigos/Assets/Scripts/Bullet.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour {
+
+   [SerializeField] private Vector2 speed;
+
+	/*	void update (){
+	if(gameObject.name == "Bullet(Clone)"){
+		Destroy(gameObject, 3);
+	}
+} */
+	    void FixedUpdate(){
+        GetComponent<Rigidbody2D>().velocity = speed;
+        }
+
+	void OnTriggerEnter2D (Collider2D enemy){
+				if (enemy.CompareTag ("Enemy")) {
+						Destroy (enemy.gameObject);
+				}
+		}
+}
---
+++ Inimigos/Assets/Scripts/Manager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Manager : MonoBehaviour {
+
+   [SerializeField] private int life = 4;
+   [SerializeField] private int score = 0;
+    public GUIText textScore;
+	public GUIText textEnemyLife;
+
+    void OnTriggerEnter2D(Collider2D bullet) {
+        life -= 1;
+        Debug.Log("Vida do Iimigo = "+life);
+		textEnemyLife.text = ("Vida do Inimigo " +life.ToString());
+        if (life <= 0)
+        {
+            Debug.Log("Vida do Inimigo " + life);
+            Debug.Log("Morreu");
+            Destroy(this.gameObject);
+        }
+        score += 1;
+        Debug.Log("Meus pontos = " + score);
+        textScore.text = ("Score " +score.ToString());
+    }
+}
